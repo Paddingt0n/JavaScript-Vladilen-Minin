3bbb30d166d009615979ce88eb6927f3c50b9485
Max Basharin <max-basarin@ya.ru>
2022-01-07 16:02:34 +0300

    event objects / event delegation

--- working-with-the-DOM/adding-events/script.js
+++ working-with-the-DOM/adding-events/script.js
@@ -1,7 +1,63 @@
-let a = document.querySelector('a')
-let oldHref = a.getAttribute('href')
+let button = document.querySelector('button')
+let h1 = document.querySelector('h1')
+let input = document.querySelector('input')
 
-a.setAttribute('href', 'https://ya.ru')
-a.textContent = 'Yandex'
+let divs = document.querySelectorAll('div')
+let link = document.querySelector('a')
 
-console.log(a.getAttribute('title'))
+// let p = document.querySelectorAll('p')
+
+// for (let i = 0; i < p.length; i++) {
+//     p[i].addEventListener('click', function(event) {
+//         event.target.style.color = 'red'
+//     })
+// }
+
+document.getElementById('wrapper').addEventListener('click', function(event) {
+    let tagName = event.target.tagName.toLowerCase()
+
+    if (tagName === 'p') {
+        event.target.style.color = 'blue'
+    }
+})
+
+
+
+for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', function(event) {
+        event.stopPropagation() // останаливает событие на элементе по которому кликнули
+        console.log(this.getAttribute('id'))
+    }, true)
+}
+
+function buttonHandler() {
+    console.log('click')
+    h1.textContent = input.value
+}
+
+h1.addEventListener('mouseenter', function() {
+    this.style.color = 'red'
+    console.log('this')
+})
+
+button.addEventListener('click', buttonHandler)
+
+link.addEventListener('click', handleLinkClick)
+
+function handleLinkClick(event) {
+    event.preventDefault()
+
+    let div = divs[0]
+
+    // if (div.style.display === 'none') {
+    //     div.style.display = 'flex'
+    // } else {
+    //     div.style.display = 'none'
+    // }
+
+    div.style.display = div.style.display === 'none'
+     ? 'flex'
+     : 'none'
+
+    console.log(div.style.display)
+}
